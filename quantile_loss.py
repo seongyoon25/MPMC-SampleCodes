05712f82665f4d51a37227fda9db4479aa62e83a
seongyoon25 <seongyoonk25@gmail.com>
2022-09-16 11:28:38 +0900

    add function to get CI

--- quantile_loss.py
+++ quantile_loss.py
@@ -1,17 +1,18 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 class Model(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, num_quantiles):
         super(Model, self).__init__()
 
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.output_size = output_size
-        self.num_quantiles = num_quantiles
+        self.input_size = input_size  # input size
+        self.hidden_size = hidden_size  # any hidden size
+        self.output_size = output_size  # target size
+        self.num_quantiles = num_quantiles  # number of quantiles
 
-        self.out = nn.Sequential(
+        self.out = nn.Sequential(  # any model
             nn.Linear(self.input_size, self.hidden_size),
             ...,
             nn.Linear(self.hidden_size, self.output_size*self.num_quantiles)
@@ -47,9 +48,17 @@ class QuantileLoss(nn.Module):
             y_pred = y_pred[..., idx]
         return y_pred
 
+    def to_interval(self, y_pred: torch.Tensor) -> torch.Tensor:
+        if y_pred.ndim == 3:
+            idx = self.quantiles.index(0.025)
+            y_pred_lower = y_pred[..., idx]
+            idx = self.quantiles.index(0.975)
+            y_pred_upper = y_pred[..., idx]
+        return y_pred_lower, y_pred_upper
+
 
 model = Model(...)
-quantiles = [0.02, 0.1, 0.25, 0.5, 0.75, 0.9, 0.98]
+quantiles = [0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975]
 criterion = QuantileLoss(quantiles)
 trainloader = ...
 for X_batch, Y_batch in trainloader:
@@ -57,4 +66,8 @@ for X_batch, Y_batch in trainloader:
     out = model(X_batch)
     loss = criterion(out, Y_batch)
 
-    out_numpy = criterion.to_prediction(out.detach().cpu()).numpy()
+    # get median prediction
+    out_pred = criterion.to_prediction(out.detach().cpu())
+
+    # get 95% confidence interval
+    out_lower, out_upper = criterion.to_interval(out.detach().cpu())
